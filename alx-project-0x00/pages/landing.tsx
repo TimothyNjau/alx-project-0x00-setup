bab8ca1d54ca2daa70779be69ea38c6ad27e1201
T1mothy Nj4u <93652350+TimothyNjau@users.noreply.github.com>
2025-07-16 14:35:34 +0300

    feat: enhance Landing component layout and add multiple Card instances

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,12 @@
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
     return (
-        <div>
+        <div className="flex flex-col gap-10 justify-items bg-gray-700">
             <h1 className="text-xl font-extralight">Landing Page</h1>
+            <Card />
+            <Card />
+            <Card />
         </div>
     )
 }
